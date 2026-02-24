cfbd8de35671b16d5d53db05e4275e1e86e201cc
Chris Nd <chrisndouassi@gmail.com>
2026-02-24 19:51:35 +0100

    prototypage d'un feu tricolore et d'un feu de passage piéton

---
+++ Traffic_light_and_crosswalk/Traffic_light_and_crosswalk.ino
@@ -0,0 +1,55 @@
+const int trafficRedLedPin = 13;
+const int trafficOrangeLedPin = 12;
+const int trafficGreenLedPin = 8;
+
+const int buttonPin = 7;
+
+const int walkRedLedPin = 4;
+const int walkGreenLedPin = 2;
+
+
+int buttonState = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trafficRedLedPin, OUTPUT);
+  pinMode(trafficOrangeLedPin, OUTPUT);
+  pinMode(trafficGreenLedPin, OUTPUT);
+
+  pinMode(buttonPin, INPUT);
+
+  pinMode(walkRedLedPin, OUTPUT);
+  pinMode(walkGreenLedPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(buttonState == HIGH) {
+    digitalWrite(walkRedLedPin, LOW);
+    digitalWrite(walkGreenLedPin, HIGH);
+    delay(3000);
+    digitalWrite(walkGreenLedPin, LOW);
+
+    buttonState = digitalRead(buttonPin);
+  } else {
+    digitalWrite(walkRedLedPin, HIGH);
+
+    digitalWrite(trafficRedLedPin, HIGH);
+    delay(5000);
+    digitalWrite(trafficRedLedPin, LOW);
+
+    for(int i = 1; i <= 3; i++) {
+      digitalWrite(trafficOrangeLedPin, HIGH);
+      delay(500);
+      digitalWrite(trafficOrangeLedPin, LOW);
+      delay(500);
+    }
+
+    digitalWrite(trafficGreenLedPin, HIGH);
+    delay(5000);
+    digitalWrite(trafficGreenLedPin, LOW);
+
+    buttonState = digitalRead(buttonPin);
+  }
+}
