d569d0782f6b01860b6924b4815e99d08cef99d6
Chris Nd <chrisndouassi@gmail.com>
2026-02-25 13:49:31 +0100

    utilisation d'une photorésistance avec monitoring de données

---
+++ Photoresitor/Photoresitor.ino
@@ -0,0 +1,26 @@
+const int analogInPin = A0;
+const int analogOutLedPIn = 9;
+
+int sensorValue = 0;
+
+int outLedValue = 0;
+
+void setup() {
+  pinMode(analogInPin, INPUT);
+  pinMode(analogOutLedPIn, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(analogInPin);
+  //outLedValue = (sensorValue * 255) / 1024;
+  outLedValue = map(sensorValue, 0, 1024, 0, 255);
+
+  analogWrite(analogOutLedPIn, outLedValue);
+
+  Serial.print("La valeur du capteur = ");
+  Serial.print(sensorValue);
+  Serial.print("\t La sortie = ");
+  Serial.println(outLedValue);
+}
